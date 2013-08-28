085db366810b44f58b134fee941cbf636fc01674
Tom Barclay <tbarclay@pacbell.net>
2013-08-28 11:27:56 -0700

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Tom Barclay on 8/28/13.
+//  Copyright (c) 2013 Oceane Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Tom Barclay on 8/28/13.
+//  Copyright (c) 2013 Oceane Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
